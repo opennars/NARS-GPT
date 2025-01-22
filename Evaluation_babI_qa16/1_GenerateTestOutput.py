6450f848e4ecd2b38baad74335fe2338caab75c3
Patrick Hammer <patham9@gmail.com>
2025-01-22 16:49:08 +0000

    Update: the script update for qa15 babI support

--- Evaluation_babI_qa16/1_GenerateTestOutput.py
+++ Evaluation_babI_qa16/1_GenerateTestOutput.py
@@ -2,7 +2,7 @@ import sys
 import os
 
 lines = ""#"../../babI/task_16_v1-2/en-valid/qa16_test.txt"
-with open("../../babI/task_16_v1-2/en-valid/qa16_train.txt") as f:
+with open("/Users/patham9/babI/tasks_1-20_v1-2/en/qa15_basic-deduction_test.txt") as f: #qa16_basic-induction_test.txt #qa15_basic-deduction_test.txt
     lines = f.read().split("\n")
 
 cwd = os.getcwd()
@@ -27,21 +27,27 @@ for line in lines:
     lastnum = number
 examples.append(example_cur)
 
-def question_and_expected_output(questionline):
-    splitted = questionline.split("\t")
-    return [splitted[0], splitted[1]]
+def question_and_expected_output(questionlines):
+    L = []
+    for questionline in questionlines:
+        splitted = questionline.split("\t")
+        L.append([splitted[0], splitted[1]])
+    return [L]
 
-examples = [[" ".join(example[0:-1])] + question_and_expected_output(example[-1]) for example in examples]
+examples = [[" ".join([x for x in example if "?" not in x])] + question_and_expected_output([x for x in example if "?" in x]) for example in examples]
+#print(examples); exit(0)
 
 Line_Input_Output_ExpectedOutput = []
 ExampleID = 1
 for example in examples:
-    BeliefInput, QuestionInput, expectedOutput = example
-    expectedOutput = expectedOutput.strip()
+    #print(example); exit(0)
+    BeliefInput, QuestionInputs_expectedOutputs = example
     outputFromBeliefInput = AddInput(BeliefInput, Print=False, PrintInputSentenceOverride=True, PrintInputSentenceOverrideValue=True)["GPT_Answer"]
-    actualOutput = AddInput(QuestionInput, Print=False, PrintInputSentenceOverride=True, PrintInputSentenceOverrideValue=True)["GPT_Answer"]
-    Dic = {"ExampleID": ExampleID, "Input": BeliefInput + " " + QuestionInput, "OutputFromBeliefInput": outputFromBeliefInput,"actualOutput": actualOutput, "expectedOutput": expectedOutput}
-    Line_Input_Output_ExpectedOutput.append(Dic)
+    for QuestionInput, expectedOutput in QuestionInputs_expectedOutputs:
+        expectedOutput = expectedOutput.strip()
+        actualOutput = AddInput(QuestionInput, Print=False, PrintInputSentenceOverride=True, PrintInputSentenceOverrideValue=True)["GPT_Answer"]
+        Dic = {"ExampleID": ExampleID, "Input": BeliefInput + " " + QuestionInput, "OutputFromBeliefInput": outputFromBeliefInput,"actualOutput": actualOutput, "expectedOutput": expectedOutput}
+        Line_Input_Output_ExpectedOutput.append(Dic)
     for k in Dic:
         print(k+":", Dic[k])
     print("\n")
